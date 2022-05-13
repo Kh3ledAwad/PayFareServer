af48e295dc46377470b88eca0a3ba9f90c12a359
Kh3ledAwad <khaledawad1200@gmail.com>
2022-05-13 07:26:01 +0200

    Fix Error

---
+++ src/main/java/com/payment/payfareserver/dto/BlockDTO.java
@@ -0,0 +1,96 @@
+package com.payment.payfareserver.dto;
+
+import com.payment.payfareserver.utilities.SHA256Helper;
+
+public class BlockchainDTO {
+    private  Integer id;
+    private  String hash;
+    private  String previousHash;
+    private  TripDTO trip;
+    private  Integer nonce=0;
+
+    private String data;
+
+    public BlockchainDTO(Integer id, String hash, String previousHash, TripDTO trip) {
+        this.id = id;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.trip = trip;
+    }
+
+    public BlockchainDTO() {
+    }
+    public void generateHash() {
+
+        String dataToHash =  previousHash + Integer.toString(nonce) + data.toString();
+
+        String hashValue = SHA256Helper.generateHash(dataToHash);
+
+        this.hash = hashValue;
+
+    }
+    public void incrementNonce() {
+
+        this.nonce++;
+    }
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public TripDTO getTrip() {
+        return trip;
+    }
+
+
+    public Integer getNonce() {
+        return nonce;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public void setTrip(TripDTO trip) {
+        this.trip = trip;
+    }
+
+
+    public void setNonce(Integer nonce) {
+        this.nonce = nonce;
+    }
+
+    @Override
+    public String toString() {
+        return "BlockchainDto{" +
+                "id=" + id +
+                ", hash='" + hash + '\'' +
+                ", previousHash='" + previousHash + '\'' +
+                ", trip=" + trip +
+                ", nonce=" + nonce +
+                '}';
+    }
+}
